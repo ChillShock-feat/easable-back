9f6e0dea7eafea0f9f755a23de54533975c9c7ad
Ryota-nino <ryota.swimmer@icloud.com>
2021-06-02 13:51:28 +0900

    rm ignore sever.php

---
+++ config/database.php
@@ -0,0 +1,5 @@
+<?php
+define('DBUSER', 'test');
+define('DBPASSWORD', 'Password_820');
+define('DBSERVER', '192.168.56.101');
+define('DBNAME', 'easable_test');
--- config/server.php
+++
@@ -1,3 +0,0 @@
-<?php
-define('WEB_SERVER', 'http://localhost/easable-app');
-define('AP_SERVER', 'http://192.168.56.101/easable-back');
