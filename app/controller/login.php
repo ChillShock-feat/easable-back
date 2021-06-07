1d346cef73545ad9ad0b4785110d3679acdedc13
toasahi <asa1313to@gmail.com>
2021-06-07 12:15:42 +0900

    Login処理　変更

--- app/controller/login.php
+++ app/controller/login.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 require_once(dirname(__FILE__) . '/../model/database_func.php');
 require_once(dirname(__FILE__) . '/../../config/server.php');
 
@@ -7,8 +8,6 @@ $DB_function = new DBFunction;
 $pdo = $DB_function->DB_connect();
 
 if (isset($_POST['submit'])) {
-    $DB_function->userLogin($pdo,$_POST['email'],$_POST['password']);
-    
     if($DB_function->userLogin($pdo,$_POST['email'],$_POST['password'])){
         header("Location:http://" . WEB_SERVER . "/easable-app/registration_sample/home.php");
     }else{
